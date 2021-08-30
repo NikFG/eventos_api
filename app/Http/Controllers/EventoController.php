5c30ffc526c4d12d406e063c5169e7a016356a26
Nikollas <nikollasferreirag@gmail.com>
2021-08-30 14:46:38 -0300

    feat:cria controller para eventos

---
+++ app/Http/Controllers/EventoController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Evento;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class EventoController extends Controller {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse {
+        $eventos = Evento::with(['atividades' => function ($query) {
+            $query->orderBy('data')->orderBy('horario_inicio')->orderBy('horario_fim')->orderBy("nome");
+        }])->with('imagens')->with('categoria')->get();
+        return response()->json($eventos);
+    }
+
+    public function porCategoria($id): JsonResponse {
+        $eventos = Evento::with(['atividades' => function ($query) {
+            $query->orderBy('data')->orderBy('horario_inicio')->orderBy('horario_fim')->orderBy("nome");
+        }])->with('imagens')->with('categoria')->whereRelation('categoria', 'id', '=', $id)->get();
+        return response()->json($eventos);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request) {
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function show($id) {
+        $evento = Evento::find($id)->with(['atividades' => function ($query) {
+            $query->orderBy('data')->orderBy('horario_inicio')->orderBy('horario_fim')->orderBy("nome");
+        }])->with('imagens')->get();
+        return response()->json($evento);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id) {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id) {
+        //
+    }
+}
