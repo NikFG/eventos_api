c1318eebe0ceca2d64e597a5db26d798a9b1e660
Nikollas <nikollasferreirag@gmail.com>
2021-08-30 14:46:50 -0300

    feat: cria controller para user

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function store(Request $request): JsonResponse {
+        $validator = Validator::make($request->all(), [
+            'nome' => 'required|string',
+            'email' => 'required|email',
+            'cpf' => 'required|cpf',
+            'password' => 'required|min:6|string',
+            'telefone' => 'required|celular_com_ddd',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+        $user = User::where('email', $request->email)->first();
+        if ($user != null) {
+            return response()->json([], 409);
+        }
+        $user = new User();
+        $user->nome = $request->nome;
+        $user->cpf = $request->cpf;
+        $user->password = bcrypt($request->password);
+        $user->email = $request->email;
+        $user->telefone = $request->telefone;
+        $user->save();
+        return response()->json('Login criado com sucesso! Verifique seu email', 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id) {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id) {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id) {
+        //
+    }
+}
