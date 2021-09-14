33f878f19858a67eedb1d39cfee6783078299083
Nikollas <nikollasferreirag@gmail.com>
2021-09-14 16:51:41 -0300

    feat: cria função para pesquisar eventos
    Esta é baseada no nome, na breve descrição ou na categoria pesquisada

--- app/Http/Controllers/EventoController.php
+++ app/Http/Controllers/EventoController.php
@@ -74,11 +74,33 @@ class EventoController extends Controller {
      * @param int $id
      * @return JsonResponse
      */
-    public function show($id) {
+    public function show($id): JsonResponse {
         $evento = Evento::with(['atividades' => function ($query) {
             $query->orderBy('data')->orderBy('horario_inicio')->orderBy('horario_fim')->orderBy("nome");
         }])->find($id);
         return response()->json($evento);
+    }
+
+    public function search($query): JsonResponse {
+
+        $unwanted_array = array('Š' => 'S', 'š' => 's', 'Ž' => 'Z', 'ž' => 'z', 'À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä' => 'A', 'Å' => 'A', 'Æ' => 'A', 'Ç' => 'C', 'È' => 'E', 'É' => 'E',
+            'Ê' => 'E', 'Ë' => 'E', 'Ì' => 'I', 'Í' => 'I', 'Î' => 'I', 'Ï' => 'I', 'Ñ' => 'N', 'Ò' => 'O', 'Ó' => 'O', 'Ô' => 'O', 'Õ' => 'O', 'Ö' => 'O', 'Ø' => 'O', 'Ù' => 'U',
+            'Ú' => 'U', 'Û' => 'U', 'Ü' => 'U', 'Ý' => 'Y', 'Þ' => 'B', 'ß' => 'Ss', 'à' => 'a', 'á' => 'a', 'â' => 'a', 'ã' => 'a', 'ä' => 'a', 'å' => 'a', 'æ' => 'a', 'ç' => 'c',
+            'è' => 'e', 'é' => 'e', 'ê' => 'e', 'ë' => 'e', 'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i', 'ð' => 'o', 'ñ' => 'n', 'ò' => 'o', 'ó' => 'o', 'ô' => 'o', 'õ' => 'o',
+            'ö' => 'o', 'ø' => 'o', 'ù' => 'u', 'ú' => 'u', 'û' => 'u', 'ý' => 'y', 'þ' => 'b', 'ÿ' => 'y');
+
+        $str = strtr($query, $unwanted_array);
+        $queryLower = trim(strtolower($str));
+        $eventos = Evento::with(['atividades' => function ($query) {
+            $query->orderBy('data')->orderBy('horario_inicio')->orderBy('horario_fim')->orderBy("nome");
+        }])
+            ->where(DB::raw('lower(nome)'), 'like', '%' . $queryLower . '%')
+            ->orWhere(DB::raw('lower(breve_descricao)'), 'like', '%' . $queryLower . '%')
+            ->orWhereHas('categoria', function ($q) use ($queryLower) {
+                $q->where(DB::raw('lower(categorias.nome)'), 'like', '%' . $queryLower . '%');
+            })
+            ->get();
+        return response()->json($eventos);
     }
 
     /**
