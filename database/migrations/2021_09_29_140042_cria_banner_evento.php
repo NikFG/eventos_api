3a6076ff098ae260cfa381a03ad246f2672bdb8f
Nikollas <nikollasferreirag@gmail.com>
2021-09-30 15:18:10 -0300

    feat: cria migration para banner

---
+++ database/migrations/2021_09_29_140042_cria_banner_evento.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CriaBannerEvento extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::table('eventos', function (Blueprint $table) {
+            $table->string('banner', 300)->nullable()->default("");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::table('eventos', function (Blueprint $table) {
+            $table->dropColumn('banner');
+        });
+    }
+}
