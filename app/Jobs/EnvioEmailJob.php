87a3faf6c7ae0163ee76af97ab8c0d19ef6aa4b8
Nikollas <nikollasferreirag@gmail.com>
2021-11-10 10:59:30 -0300

    feat: padroniza job e mail de envio

--- app/Jobs/EnvioEmailJob.php
+++ app/Jobs/EnvioEmailJob.php
@@ -2,6 +2,9 @@
 
 namespace App\Jobs;
 
+use App\Mail\EnvioEmail;
+use App\Models\Certificado;
+use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -9,19 +12,24 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Mail;
 
-class EnvioEmail implements ShouldQueue {
+class EnvioEmailJob implements ShouldQueue {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     public $tries = 3;
+    public $timeout = 500;
+    public $maxExceptions = 2;
     private $user;
+    private $certificado;
+
 
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct(\stdClass $user) {
+    public function __construct(User $user, Certificado $certificado) {
         $this->user = $user;
+        $this->certificado = $certificado;
     }
 
     /**
@@ -30,7 +38,6 @@ class EnvioEmail implements ShouldQueue {
      * @return void
      */
     public function handle() {
-        Mail::send(new \App\Mail\EnvioEmail($this->user));
-
+        Mail::send(new EnvioEmail($this->user, $this->certificado));
     }
 }
