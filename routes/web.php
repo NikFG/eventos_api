0cc0724e8ca20d312bcedf6bbe1c6c8b9198fcef
Nikollas <nikollasferreirag@gmail.com>
2021-11-12 14:30:19 -0300

    feat: cria comentarios para agregar a script

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,12 @@
 <?php
 
 use App\Http\Controllers\VerificationController;
-use App\Mail\EnvioEmail;
 use App\Models\Atividade;
 use App\Models\Certificado;
 use App\Models\Instituicao;
 use App\Models\ModeloCertificado;
 use App\Models\User;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 
@@ -24,6 +22,71 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+//Permissões
+    /* $usuario = Role::create(['name' => 'usuario', 'guard_name' => 'api']);
+     $associado = Role::create(['name' => 'associado', 'guard_name' => 'api']);
+     $admin = Role::create(['name' => 'admin', 'guard_name' => 'api']);
+     $superadmin = Role::create(['name' => 'super-admin', 'guard_name' => 'api']);
+
+     $g_usuario = Permission::create(['name' => 'gerenciar_usuario', 'guard_name' => 'api']);
+
+
+     $v_certificado = Permission::create(['name' => 'visualizar_certificado', 'guard_name' => 'api']);
+     $g_certificado = Permission::create(['name' => 'gerenciar_certificado', 'guard_name' => 'api']);
+
+
+     $e_evento = Permission::create(['name' => 'entrar_evento', 'guard_name' => 'api']);
+     $g_evento = Permission::create(['name' => 'gerenciar_evento', 'guard_name' => 'api']);
+
+
+     $c_instituicao = Permission::create(['name' => 'cadastrar_instituicao', 'guard_name' => 'api']);
+     $g_instituicao = Permission::create(['name' => 'gerenciar_instituicao', 'guard_name' => 'api']);
+
+
+
+     $usuario->givePermissionTo($g_usuario);
+     $usuario->givePermissionTo($v_certificado);
+     $usuario->givePermissionTo($e_evento);
+     $usuario->givePermissionTo($c_instituicao);
+
+     $associado->givePermissionTo($g_usuario);
+     $associado->givePermissionTo($v_certificado);
+     $associado->givePermissionTo($e_evento);
+     $associado->givePermissionTo($c_instituicao);
+     $associado->givePermissionTo($g_certificado);
+     $associado->givePermissionTo($g_evento);
+
+
+     $admin->givePermissionTo($g_usuario);
+     $admin->givePermissionTo($v_certificado);
+     $admin->givePermissionTo($e_evento);
+     $admin->givePermissionTo($c_instituicao);
+     $admin->givePermissionTo($g_certificado);
+     $admin->givePermissionTo($g_evento);
+     $admin->givePermissionTo($g_instituicao);
+
+    $c = new \App\Models\Categoria();
+    $c->nome = 'teste';
+    $c->save();
+
+    $ti = new \App\Models\TipoImagem();
+    $ti->nome = "teste";
+    $ti->save();
+
+    $ti = new \App\Models\TipoImagem();
+    $ti->nome = "teste2";
+    $ti->save();
+
+    $ta = new \App\Models\TipoAtividade();
+    $ta->nome = "teste";
+    $ta->save();
+
+    $te = new \App\Models\TipoEvento();
+    $te->nome = "teste";
+    $te->save();
+    $user = User::find(1);
+    $user->assignRole('super-admin');*/
+
     return view('welcome');
 });
 
@@ -31,7 +94,7 @@ Route::get('email/verify/{id}', [VerificationController::class, 'verify'])->name
 Route::get('email/resend', [VerificationController::class, 'resend'])->name('verification.resend');
 Route::view('forgot_password', 'auth.reset_password')->name('password.reset');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 //certificado route with certicidao view
 Route::get('/certificado', function () {
     $user = User::findOrFail(1);
@@ -52,9 +115,9 @@ Route::get('/certificado', function () {
         'data' => $data,
 
     ])->setPaper('a4', 'landscape')->setWarnings(false);
-  return  $pdf->download('teste.pdf');
+    return $pdf->download('teste.pdf');
 
-    return view('certificado',[
+    return view('certificado', [
         'certificado' => $c,
         'modelo' => $modelo,
         'participante' => $user,
