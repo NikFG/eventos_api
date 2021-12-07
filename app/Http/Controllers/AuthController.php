4aed0ab1e0d9bbc8ca42baee75968834efefaa04
Nikollas <nikollasferreirag@gmail.com>
2021-12-07 10:11:01 -0300

    feat: envia roles do usuario ao fazer login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -28,6 +28,7 @@ class AuthController extends Controller {
                 'access_token' => $token,
                 'token_type' => 'bearer',
                 'expires_in' => auth('api')->factory()->getTTL() * 60,
+                'roles' => $user->getRoleNames(),
                 'user' => $user,
             ]);
         }
